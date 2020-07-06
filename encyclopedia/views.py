be6b690fde248a5b5f1fb007b42cafd9af99fa9f
bruno okamoto <brunookamoto11@gmail.com>
2020-07-06 05:49:54 -0300

    add entry and search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
 
 from . import util
 
@@ -8,3 +10,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def wiki(request, title):
+    if util.get_entry(title) == None:
+        raise Http404("Title does not exist")
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title),
+        "title": title,
+    })
+
+def search(request):
+    query = request.POST.get('q','')
+    if query:
+        return HttpResponseRedirect(reverse('wiki', args=(query,)))
+    else:
+        return HttpResponseRedirect(reverse('index'))
