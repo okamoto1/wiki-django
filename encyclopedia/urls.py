2df4ab0629fe705ddba0cdb3fdd4738daf767065
bruno okamoto <brunookamoto11@gmail.com>
2020-07-07 02:37:48 -0300

    adding new page and random

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-    path("wiki/", views.search, name="search")
+    path("wiki/", views.search, name="search"),
+    path("create-entry", views.create_entry, name="create_entry"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("ramdom", views.randompick, name="random"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from markdown2 import Markdown
+import random
 
 from . import util
 
@@ -11,10 +13,12 @@ def index(request):
     })
 
 def wiki(request, title):
+    markdowner = Markdown()
     if util.get_entry(title) == None:
         raise Http404("Title does not exist")
+    markdown = markdowner.convert(util.get_entry(title))
     return render(request, "encyclopedia/entry.html", {
-        "entry": util.get_entry(title),
+        "entry": markdown,
         "title": title,
     })
 
@@ -24,3 +28,28 @@ def search(request):
         return HttpResponseRedirect(reverse('wiki', args=(query,)))
     else:
         return HttpResponseRedirect(reverse('index'))
+
+def create_entry(request):
+    new_title = request.POST.get('title')
+    content = request.POST.get('content')
+    entries = [x.lower() for x in util.list_entries()]
+    if new_title:
+        if new_title.lower() in entries:
+            raise Http404("Title does not exist")
+        else:
+            util.save_entry(new_title, content)
+            return HttpResponseRedirect(reverse('wiki', args=(new_title,)))
+    return render(request, "encyclopedia/new-entry.html")
+
+def edit(request, title):
+    content = request.POST.get('content')
+    if content:
+        util.save_entry(title, content)
+        return HttpResponseRedirect(reverse('wiki', args=(title,)))
+    return render(request, "encyclopedia/edit-entry.html", {
+        "title": title
+    })
+
+def randompick(request):
+    random_title = random.choice(util.list_entries())
+    return HttpResponseRedirect(reverse('wiki', args=(random_title,)))
